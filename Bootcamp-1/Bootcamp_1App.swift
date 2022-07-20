09fda4b8de8fa49961ff2cbca6042c50611d8e3f
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-20 22:56:29 +0300

    Initial Commit

---
+++ Bootcamp-1/Bootcamp_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bootcamp_1App.swift
+//  Bootcamp-1
+//
+//  Created by Ömer Faruk Kılıçaslan on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Bootcamp_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
